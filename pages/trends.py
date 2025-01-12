d53b215ac93610e66944a843bf7eafd3f20e5fab
Anna Rácz <aracz@Annas-MacBook-Air.local>
2025-01-12 14:25:00 +0100

    updated readme and added dow 30 companies

--- pages/trends.py
+++ pages/trends.py
@@ -18,3 +18,36 @@ st.write(data)
 # eg. Dow 30 companies vs the dow 30 index
 
 # Price trends in 1 sector
+
+dow_30_symbols = [
+    "MMM",  # 3M Company
+    "AXP",  # American Express
+    "AMGN", # Amgen
+    "AAPL", # Apple Inc.
+    "BA",   # Boeing
+    "CAT",  # Caterpillar Inc.
+    "CVX",  # Chevron
+    "CSCO", # Cisco Systems
+    "KO",   # Coca-Cola Company
+    "DIS",  # Disney (The Walt Disney Company)
+    "DOW",  # Dow Inc.
+    "GS",   # Goldman Sachs
+    "HD",   # Home Depot
+    "HON",  # Honeywell International
+    "IBM",  # IBM
+    "INTC", # Intel Corporation
+    "JNJ",  # Johnson & Johnson
+    "JPM",  # JPMorgan Chase & Co.
+    "MCD",  # McDonald's
+    "MRK",  # Merck & Co.
+    "MSFT", # Microsoft
+    "NKE",  # Nike
+    "PFE",  # Pfizer
+    "PG",   # Procter & Gamble
+    "RTX",  # Raytheon Technologies
+    "CRM",  # Salesforce
+    "TRV",  # The Travelers Companies
+    "UNH",  # UnitedHealth Group
+    "VZ",   # Verizon Communications
+    "V"     # Visa Inc.
+]
