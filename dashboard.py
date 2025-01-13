4907a4d2b193fbf6c1f2c15d4f503cd081840cf6
Anna Rácz <aracz@Annas-MacBook-Air.local>
2025-01-13 13:23:19 +0100

    added dow30 to main page

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,9 @@ import streamlit as st
 home = st.Page("pages/home.py", title="Bevezető", icon=":material/house:")
 trends = st.Page("pages/trends.py", title="Trendek", icon=":material/stacked_line_chart:")
 events = st.Page("pages/events.py", title="Események", icon=":material/globe:")
+dow30 = st.Page("pages/dow30.py", title="Dow 30 Index", icon=":material/globe:")
 
-pg = st.navigation([home, trends, events])
+pg = st.navigation([home, trends, events, dow30])
 st.set_page_config(page_title="Tőzsdei Adatok", page_icon=":material/edit:")
 pg.run()
 
