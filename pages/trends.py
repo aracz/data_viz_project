f8270083db07ac2fcca64e8d48e8af3b8e82fb7d
Mészáros Anna Veronika <meszaros.annapanni@gmail.com>
2025-01-13 15:32:18 +0100

    Refactor trends page

--- pages/trends.py
+++ pages/trends.py
@@ -10,18 +10,6 @@ def data_prep():
     print(stock_data)
     return stock_data
 
-
-data = data_prep()
-
-st.title("Trendek")
-st.write("Teszt adatmegjelnites")
-st.write(data)
-
-# Index vs underlying companies values
-# eg. Dow 30 companies vs the dow 30 index
-
-# Price trends in 1 sector
-
 dow_30_symbols = [
     "MMM",  # 3M Company
     "AXP",  # American Express
@@ -54,38 +42,25 @@ dow_30_symbols = [
     "VZ",   # Verizon Communications
     "V"     # Visa Inc.
 ]
+def get_dow30(period="1y"):
+    dow30 = []
+    for symbol in dow_30_symbols:
+        stock = get_stock_data.GetStockData(symbol, period=period)
+        df = stock.get_stock_data_period()
+        df["yy-mm"] = pd.to_datetime(df.index.strftime('%Y-%m'))
+        df["symbol"] = symbol
+        df["Sector"] = stock.get_stock_info("sector")
+        df["Volatility"] = 200 * (df["High"] - df["Low"]) / (df["High"] + df["Low"])
+        dow30.append(df)
+    dow30 = pd.concat(dow30)
+    return dow30
 
-dow30_10y = []
-for code in dow_30_symbols:
-    stock = get_stock_data.GetStockData(code, period="5y")
-    df = stock.get_stock_data_period()
-    df["yy-mm"] = pd.to_datetime(df.index.strftime('%Y-%m'))
-    df["Code"] = code
-    df["Sector"] = stock.get_stock_info("sector")
-    df["Volatility"] = 200 * (df["High"] - df["Low"]) / (df["High"] + df["Low"])
-    dow30_10y.append(df)
-dow30_10y = pd.concat(dow30_10y)
-
-# Dow 30 companies by sector in the last 5 years
-fig = plt.figure()
-sb.lineplot(data=dow30_10y, x="yy-mm", y="Close", hue="Sector", errorbar=None)
-st.write(fig)
-
-# Volatility of stocks in some sectors of dow 30
-secs = ["Industrials", "Financial Services", "Technology"]
-fig = plt.figure()
-sb.lineplot(data=dow30_10y[dow30_10y["Sector"].isin(secs)].groupby(["yy-mm", "Sector"]).mean(numeric_only=True), 
-            x="yy-mm", y="Volatility", errorbar=None, hue="Sector")
-st.write(fig)
-
-monthly = dow30_10y.groupby(["yy-mm", "Sector"])
-monthly_vol = (200 * (monthly.max("High")["High"] - monthly.min("Low")["Low"]) 
+def get_monthly_volatility(df):
+    monthly = df.groupby(["yy-mm", "Sector"])
+    monthly_vol = (200 * (monthly.max("High")["High"] - monthly.min("Low")["Low"]) 
                / (monthly.max("High")["High"] + monthly.min("Low")["Low"])).reset_index()
-fig = plt.figure()
-sb.lineplot(data=monthly_vol[monthly_vol["Sector"].isin(secs)], x="yy-mm", y=0, errorbar=None, hue="Sector")
-st.write(fig)
+    return monthly_vol
 
-# Dividends vs stock price
 def hilo_and_divs(df, name=None):
     hl = df[["High", "Low"]].reset_index().melt(id_vars="Date")
     divs = df[["Stock Splits", "Dividends"]].reset_index().melt(id_vars="Date")
@@ -95,22 +70,21 @@ def hilo_and_divs(df, name=None):
     divs["Name"] = name
     return hl, divs
 
-def draw_hilo_divs(code):
-    stock = get_stock_data.GetStockData(code, period="1y")
+def draw_hilo_divs(symbol):
+    stock = get_stock_data.GetStockData(symbol, period="1y")
     sname = stock.get_stock_info("shortName")
     hilo, divs = hilo_and_divs(stock.get_stock_data_period(), name=sname)
     fig = plt.figure(figsize=(10, 6))
     sb.lineplot(data=hilo, x="Date", y="value", hue="variable")
     sb.scatterplot(data=divs, x="Date", y="y", hue="variable", size="value")
-    plt.title(f"{sname} ({code}) stock price and dividends")
+    plt.title(f"{sname} ({symbol}) árfolyam és osztalék")
+    plt.xticks(rotation=45)
+    plt.ylabel("Érték (USD)")
+    plt.xlabel("Dátum")
     return fig
 
-st.write("Nagy tech cégek részvényárfolyamának alakulása és osztalékai") 
-st.write(draw_hilo_divs("IBM"))
-st.write(draw_hilo_divs("AAPL"))
-
-def candlesticks(df):
-    stock = get_stock_data.GetStockData(code, period="3mo")
+def candlesticks(symbol):
+    stock = get_stock_data.GetStockData(symbol, period="3mo")
     sname = stock.get_stock_info("shortName")
     df = stock.get_stock_data_period()
     up = df[df["Close"] > df["Open"]]
@@ -122,14 +96,53 @@ def candlesticks(df):
     plt.bar(down.index, down.High - down.Low, width=0.1, bottom=down.Low, color="red")
     plt.bar(down.index, down.Open - down.Close, bottom=down.Close, color="red")
     #volumes
-    min = df["Low"].min()
-    max = df["High"].max()
+    min_price = df["Low"].min()
     min_vol = df["Volume"].min()
     max_vol = df["Volume"].max()
-    scale = 0.2  * (max - min) / (max_vol - min_vol)
-    plt.bar(df.index, scale * df["Volume"], bottom=min, color="blue", alpha=0.3)
-
+    scale = 0.2  * (df["High"].max() - min_price) / (max_vol - min_vol)
+    plt.bar(df.index, scale * df["Volume"], bottom=min_price-scale*(max_vol - min_vol)/2, color="blue", alpha=0.3)
+    plt.title(f"{sname} ({symbol}) gyertyaábrája az elmúlt 3 hónapban")
+    plt.xticks(rotation=45)
+    plt.ylabel("Érték (USD)")
+    plt.xlabel("Dátum")
     return fig
 
+data = data_prep()
+
+st.title("Trendek")
+st.write("Teszt adatmegjelnites")
+st.write(data)
+
+# Dividends vs stock price
+st.subheader("Nagy tech cégek részvényárfolyamának alakulása és osztalékai")
+st.write(draw_hilo_divs("IBM"))
 st.write(candlesticks("MSFT"))
 
+# Dow 30 companies by sector
+st.subheader("A Dow 30 cégek szektorai")
+dow30_10y = get_dow30(period="10y")
+
+fig = plt.figure()
+sb.lineplot(data=dow30_10y, x="yy-mm", y="Close", hue="Sector", errorbar=None)
+plt.title(f"Dow 30 cégek részvényárfolyamának alakulása szektoronként")
+plt.ylabel("Érték (USD)")
+plt.xlabel("Dátum")
+st.write(fig)
+
+# Volatility of stocks in some sectors of dow 30
+secs = ["Industrials", "Financial Services", "Technology"]
+fig = plt.figure()
+sb.lineplot(data=dow30_10y[dow30_10y["Sector"].isin(secs)].groupby(["yy-mm", "Sector"]).mean(numeric_only=True), 
+            x="yy-mm", y="Volatility", errorbar=None, hue="Sector")
+plt.title(f"Dow 30 cégek napi volatilitása szektoronként")
+plt.ylabel("Volatilitás százalékban")
+plt.xlabel("Dátum")
+st.write(fig)
+
+monthly_vol = get_monthly_volatility(dow30_10y)
+fig = plt.figure()
+sb.lineplot(data=monthly_vol[monthly_vol["Sector"].isin(secs)], x="yy-mm", y=0, errorbar=None, hue="Sector")
+plt.title(f"Dow 30 cégek havi volatilitása szektoronként")
+plt.ylabel("Volatilitás százalékban")
+plt.xlabel("Dátum")
+st.write(fig)
